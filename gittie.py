f827406becca76bc2a3945e07d9995e7ce044314
Kamil Trąba <prooffek@gmail.com>
2020-11-07 16:50:19 +0100

    shout added

--- gittie.py
+++ gittie.py
@@ -1,10 +1,6 @@
 def introduce():
-<<<<<<< HEAD
     print("Hello, I'm Gittie")
     print("How are you?")
-=======
-    print("Hello, I'm Gittie. Nice to meet you!")
->>>>>>> 03f2a8225b9ecc1d9c9a4a6bb73894bb13928831
 
 def joke():
     user_input = False
@@ -21,3 +17,6 @@ joke()
     
 def add():
     print("add")
+
+def shout():
+    print("PLEASE, STOP SCREAMING!!!!!")
