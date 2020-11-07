94cfb0c52857e4553648ca5bf16b1f4966de0bce
Kamil Trąba <prooffek@gmail.com>
2020-11-07 16:38:09 +0100

    add function joke()

--- gittie.py
+++ gittie.py
@@ -1,3 +1,15 @@
 def introduce():
     print("Hello, I'm Gittie")
+
+def joke():
+    user_input = False
+
+    while user_input is False:
+        user_input = input("PLease tell a joke: ")
+    
+    print("HEHE Nice one!")
+
+
+joke()
+
     
